d59df0fb93bf38e2ac8b1d6acee43b010c943e76
Nguyen Cong Gioi <conggioi.pro264@gmail.com>
2024-12-15 15:12:24 +0700

    fix(driver): load weight without driver current user

--- src/features/driver-order/driver-order.service.ts
+++ src/features/driver-order/driver-order.service.ts
@@ -1,24 +1,31 @@
 import { DriverSession } from '@common/dto';
 import { OrderService } from '@features/order/order.service';
+import { RedisCacheService } from '@features/redis';
 import { Injectable, Logger } from '@nestjs/common';
 import { LessThanOrEqual } from 'typeorm';
 
 @Injectable()
 export class DriverOrderService {
   private readonly logger = new Logger(DriverOrderService.name);
-  constructor(private readonly orderService: OrderService) {}
+  constructor(
+    private readonly orderService: OrderService,
+    private readonly cacheService: RedisCacheService,
+  ) {}
 
-  getOrderPending(driver: DriverSession) {
+  async getOrderPending(driver: DriverSession) {
     this.logger.log(`Get order pending for driver ${driver.id}`);
+    const driverSession = await this.cacheService.get(`driver:${driver.id}`);
 
+    const weight = JSON.parse(driverSession)['loadWeight'];
     return this.orderService.getOrderPending(
       {
-        loadWeight: LessThanOrEqual(driver.loadWeight),
+        loadWeight: LessThanOrEqual(weight),
       },
       {
         id: true,
         pickup: { address: true, coordinates: true },
         destination: { address: true, coordinates: true },
+        totalPrice: true,
       },
     );
   }

db4fbf57c76c6d198b0aa2ae2e7bda0a887f06dc
Nguyen Cong Gioi <conggioi.pro264@gmail.com>
2024-12-17 15:47:11 +0700

    docs(api): add docs swagger for wallet history entity

--- src/features/driver-wallet/entities/wallet-history.entity.ts
+++ src/features/driver-wallet/entities/wallet-history.entity.ts
@@ -2,6 +2,7 @@ import { CoreEntity } from '@common/entities';
 import { WALLET_ACTION_ENUM, WALLET_STATUS_ENUM } from '@constants';
 import { AdminEntity } from '@features/admin/entities';
 import { DriverEntity } from '@features/driver/entities/driver.entity';
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, Index, ManyToOne, Relation } from 'typeorm';
 
 @Entity('wallet_histories')
@@ -11,19 +12,27 @@ export class WalletHistoryEntity extends CoreEntity {
     enum: WALLET_STATUS_ENUM,
     default: WALLET_STATUS_ENUM.PENDING,
   })
+  @ApiProperty({
+    enum: WALLET_STATUS_ENUM,
+    example: WALLET_STATUS_ENUM.PENDING,
+  })
   status: WALLET_STATUS_ENUM;
 
   @Column({ type: 'enum', enum: WALLET_ACTION_ENUM })
+  @ApiProperty({ enum: WALLET_ACTION_ENUM })
   action: WALLET_ACTION_ENUM;
 
   @Column({ type: 'varchar', length: 255, nullable: true })
+  @ApiProperty({ example: 'https://example.com/screenshot.png' })
   screenHot: string;
 
   @Column({ type: 'varchar', length: 255, nullable: true })
   @Index({ unique: true })
+  @ApiProperty({ example: '123456' })
   code: string;
 
   @Column()
+  @ApiProperty({ example: 100000 })
   amount: number;
 
   @ManyToOne(() => AdminEntity)

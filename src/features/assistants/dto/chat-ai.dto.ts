1dce42bddf85b905acf3a5e5780a7d388d4b786b
Gioi Nguyen <conggioi.pro264@gmail.com>
2025-01-11 16:53:59 +0700

    chore(ai): add dto for chat with ai

---
+++ src/features/assistants/dto/chat-ai.dto.ts
@@ -0,0 +1,59 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  ArrayMaxSize,
+  IsArray,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class ChatAIDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  @ApiProperty({
+    description: 'Prompt to test the function calling',
+    example: 'test',
+  })
+  prompt: string;
+
+  @IsNotEmpty()
+  @IsArray()
+  @ArrayMaxSize(10)
+  @ApiProperty({
+    description: 'History of the prompt',
+    example: [{ role: 'user', parts: [{ text: 'test' }] }],
+  })
+  history: HistoryDto[];
+}
+
+class HistoryDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  @ApiProperty({
+    description: 'Role of the history',
+    example: 'test',
+  })
+  role: 'user' | 'model';
+
+  @IsNotEmpty()
+  @IsArray()
+  @ArrayMaxSize(10)
+  @ApiProperty({
+    description: 'History of the prompt',
+    example: ['test'],
+  })
+  parts: PartDto[];
+}
+
+class PartDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100)
+  @ApiProperty({
+    description: 'Content of the part',
+    example: 'test',
+  })
+  text: string;
+}
